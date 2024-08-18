72fec30008195ac4b96d9845af8449470748ef60
Alex Cuvelier <cuvelieralex@gmail.com>
2024-08-17 23:53:53 -0700

    arrow to change quest

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,16 @@
 </template>
 <script setup>
 import OeaHeader from '@/components/OeaHeader.vue';
+
+import { useQuestsStore } from '@/stores/questsStore.js';
+const questsStore = useQuestsStore();
+
+document.onkeydown = function(e) {
+    if(e.key == 'ArrowRight') {
+        questsStore.goToNextQuest();
+    }else if(e.key == 'ArrowLeft') {
+        questsStore.goToPreviousQuest();
+    }
+}
+
 </script>
